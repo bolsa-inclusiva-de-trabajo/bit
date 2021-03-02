55e695896e94403bd4953b09a16ad9fef564ed20
Schmidt Cristian Hernán <schcriher@gmail.com>
2021-03-01 21:32:31 -0300

    Improved javascript practices

--- src/main/resources/static/public/js/scripts.js
+++ src/main/resources/static/public/js/scripts.js
@@ -1,23 +1,24 @@
-'use strict';
+const NAVBAR_HEIGHT = 100;  // pixel
+const ANIMATION_TIME = 500;  // milliseconds
 
 $(document).ready(function() {
 
     // Smooth displacement with fragments
     $('a[href^="#"]').click(function() {
         var target = $(this.hash);
-        if (target.length == 0) {
-            target = $('a[name="' + this.hash.substr(1) + '"]');
+        if (target.length === 0) {
+            target = $(`a[name="${this.hash.substr(1)}"]`);
         }
-        if (target.length == 0) {
+        if (target.length === 0) {
             target = $('html');
         }
-        $('html, body').animate({ scrollTop: target.offset().top - 100}, 500);
+        $('html, body').animate({ scrollTop: target.offset().top - NAVBAR_HEIGHT}, ANIMATION_TIME);
         return false;
     });
 
     // Hide menu on click
     $('#headerNavbar').find('a').on('click', function() {
-        if ($('#navbar-toggler-button').css('display') != 'none'){ 
+        if ($('#navbar-toggler-button').css('display') !== 'none'){
             $('#headerNavbar').collapse('toggle');
         }
     });
@@ -25,11 +26,11 @@ $(document).ready(function() {
     // Focusing on the first field in the modal
     $('#logIn').on('shown.bs.modal', function () {
         $('#logInInputEmail').trigger('focus');
-    })
+    });
 
     // Needs for validation form (works without js :)
     Array.prototype.filter.call($('.needs-validation'), function(form) {
-        form.setAttribute("novalidate", null);
+        form.setAttribute('novalidate', null);
         form.addEventListener('submit', function(event) {
             if (form.checkValidity() === false) {
                 event.preventDefault();
@@ -40,3 +41,4 @@ $(document).ready(function() {
     });
 
 });
+
